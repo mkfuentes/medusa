42a54826b867ef1474cb09f3aac8a4fedc692228
Miguel Fuentes <expansiver@gmail.com>
2020-03-27 00:32:51 -0600

    medio pinta en la venta

--- src/components/GetWithCode.jsx
+++ src/components/GetWithCode.jsx
@@ -5,6 +5,9 @@ import TextField from "@material-ui/core/TextField";
 import classes from "../assets/styles/MakeStyles";
 import Button from "@material-ui/core/Button";
 
+const data = []
+const dataClean = []
+
 export default class getWithCode extends Component {
   constructor(props) {
     super(props);
@@ -22,6 +25,7 @@ export default class getWithCode extends Component {
       barCode: e.target.value
     })
   }
+
   componentDidUpdate() {
     console.log(this.state.barCode)
   }
@@ -29,31 +33,59 @@ export default class getWithCode extends Component {
   async onClick() {
     const res = await axios.get(`http://localhost:8080/products/code/${this.state.barCode}`)
     this.setState({ product: res.data.data.code })
-    console.log(this.state.product);
+    data.push(res.data.data.code);
+    this.createTable()
+  }
+
+  createTable = () => {
+     for (let n = 0; n < data.length; n++) {
+       dataClean.push(data[n][0]);
+       n += 1;
+     }
   }
 
   render() {
     return (
       <div>
-              <TextField
-                autoComplete="false"
-                variant="outlined"
-                required
-                fullWidth
-                name="barCode"
-                label="Codigo de Barras"
-                autoFocus
-                onChange={this.onChange}
-              />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
+        <TextField
+          autoComplete="false"
+          variant="outlined"
+          required
+          fullWidth
+          name="barCode"
+          label="Codigo de Barras"
+          autoFocus
+          onChange={this.onChange}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
           color="primary"
           onClick={this.onClick}
-            >
-              Agregar Producto
-            </Button>
+        >
+          Agregar Producto
+        </Button>
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Barcode</th>
+              <th scope="col">Descripcion</th>
+              <th scope="col">Fecha</th>
+              <th scope="col">Cantidad</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataClean.map(sales => (
+              <tr>
+                <th>{sales.barCode}</th>
+                <td key={sales._id}>Temporal</td>
+                <td>{sales.fechaVenta}</td>
+                <td>{sales.cantidad}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
